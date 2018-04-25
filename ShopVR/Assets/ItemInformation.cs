f0020670b629bba54eb5b500bc4c8ad20ae9d78c
n8-webb <niallfcwebb@gmail.com>
2018-04-25 15:31:13 +0100

    Final Version

---
+++ ShopVR/Assets/ItemInformation.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using VRTK;
+
+public class ItemInformation : MonoBehaviour
+{
+    public int itemID;
+    public string displayName;
+    public TextMeshPro text;
+    public string itemName;
+    public float price;
+    public float discountedPrice;
+    public enum isle
+    {
+        dairy, bakery, meat, biscuitsSnacks, tins, teaCoffee, healthBeauty, fruitVeg, frozen
+    };
+
+    public isle isleList;
+
+    public enum discounts
+    {
+        zeroPercent, tenPercent, twentyPercent, thirtyPercent, fourtyPercent, fiftyPercent
+    };
+
+    public discounts discount;// Use this for initialization
+    void Start()
+    {
+        text = this.gameObject.GetComponentInChildren<TextMeshPro>();
+        discountedPrice = price;
+        CheckDiscount();
+
+        if (discount == discounts.zeroPercent)
+        {
+            text.text = itemName + " " + price;
+        }
+        else
+        {
+            text.text = itemName + " " + price + " Discounted to : " + discountedPrice.ToString() + "0";
+        }
+
+
+
+
+
+    }
+
+    void CheckDiscount()
+    {
+       
+        switch(discount)
+        {
+            case discounts.zeroPercent:
+                break;
+            case discounts.tenPercent:
+                discountedPrice = price * 0.9f;
+                discountedPrice = Mathf.Round(discountedPrice * 100f) / 100f;
+                break;
+            case discounts.twentyPercent:
+                discountedPrice = price * 0.8f;
+                discountedPrice = Mathf.Round(discountedPrice * 100f) / 100f;
+                break;
+            case discounts.thirtyPercent:
+                discountedPrice = price * 0.7f;
+                discountedPrice = Mathf.Round(discountedPrice * 100f) / 100f;
+                break;
+            case discounts.fourtyPercent:
+                discountedPrice = price * 0.6f;
+                discountedPrice = Mathf.Round(discountedPrice * 100f) / 100f;
+                break;
+            case discounts.fiftyPercent:
+                discountedPrice = price * 0.5f;
+                discountedPrice = Mathf.Round(discountedPrice * 100f) / 100f;
+                break;
+
+        }
+       
+    }
+
+}
+
---
+++ ShopVR/Assets/shoppingList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class shoppingList : MonoBehaviour {
+    public List<string> shopList = new List<string>();
+    public TextMeshPro itemCount;
+    public float cost = 0f;
+   
+	// Use this for initialization
+	void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject)
+        {
+            ItemInformation item = col.gameObject.GetComponent<ItemInformation>();
+            shopList.Add(item.itemName);
+            cost = cost + item.discountedPrice;
+            Destroy(col.gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        itemCount.text = "Items: " + shopList.Count.ToString() + "      Total: " + cost.ToString();
+	}
+}
